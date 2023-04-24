a88db975bcd31325720bf04285c9d0191b12085a
calheros <jo.gj10@gmail.com>
2023-04-24 20:33:16 -0300

    Create window from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,21 @@
-#include <gtk/gtk.h>
 #include <glib/gstdio.h>
+#include <gtk/gtk.h>
 
-static void
-activate(GtkApplication *app,
-         gpointer user_data)
-{
-  GtkBuilder *builder = gtk_builder_new();
-  gtk_builder_add_from_file(builder, "./window.ui", NULL);
-  GObject *window = gtk_builder_get_object( builder, "window");
-  gtk_widget_show(GTK_WIDGET(window));
-  g_object_unref(builder);
+static void activate(GtkApplication *app, gpointer user_data) {
+  GtkBuilder *builder = gtk_builder_new_from_file("src/window.ui");
+  GtkWindow *window = GTK_WINDOW(gtk_builder_get_object(builder, "window"));
+  gtk_window_present(window);
+  gtk_window_set_application(window, GTK_APPLICATION(app));
 }
 
-int main(int argc,
-         char **argv)
-{
+int main(int argc, char **argv) {
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new("br.calheiros.deskapp", G_APPLICATION_DEFAULT_FLAGS);
+  app =
+      gtk_application_new("br.calheiros.deskapp", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
-
   return status;
 }
